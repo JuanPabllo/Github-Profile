6fba885db29e71dda2c58052298524aa024bbc66
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-08 14:57:38 -0300

    num sei

--- profile/.eslintrc.js
+++ profile/.eslintrc.js
@@ -1,24 +1,36 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    es6: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
-    rules: {
-      'prettier/prettier': 'error',
-      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
-      'import/prefer-default-export': 'off',
-      'jsx-quotes': ['error', 'prefer-single'],
-    },
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
   },
-}
+};
