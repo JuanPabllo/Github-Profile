e5d627a882e9866908c24d34b2b19836a44d0682
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-09 11:42:24 -0300

    Create function to get information from dev API

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Card from './components/card';
+
 function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <div className="wrapper">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
