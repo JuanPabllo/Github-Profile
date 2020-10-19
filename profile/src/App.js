ec38b8ba5469eaf476d3b945802ec5a041b0346f
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-19 11:10:23 -0300

    Connecting with github api

--- profile/src/App.js
+++ profile/src/App.js
@@ -5,9 +5,7 @@ import Card from './components/card'
 function App() {
   return (
     <div className='wrapper'>
-      <Card userName='ben' />
       <Card userName='juanpabllo' />
-      <Card userName='graciegregory' />
     </div>
   )
 }
--- profile/src/components/card.js
+++ profile/src/components/card.js
@@ -7,9 +7,7 @@ function Card(props) {
     async function fetchData() {
       let userName = props.userName // You can write your username here
 
-      let devData = await fetch(
-        `https://dev.to/api/users/by_username?url=${userName}`
-      )
+      let devData = await fetch(`https://api.github.com/users/${userName}`)
         .then((res) => res.json())
         .then((data) => data)
       setUser(devData)
@@ -20,13 +18,13 @@ function Card(props) {
   return (
     <div className='card'>
       <div className='user-image'>
-        <img src={user.profile_image} alt='User Profile'></img>
+        <img src={user.avatar_url} alt='User Profile'></img>
       </div>
 
       <div className='user-info'>
         <div className='name'>{user.name}</div>
-        <div className='handle'>@{user.username}</div>
-        <div className='summary'>{user.summary}</div>
+        <div className='handle'>@{user.login}</div>
+        <div className='summary'>{user.bio}</div>
       </div>
 
       <div className='location'>{user.location}</div>
