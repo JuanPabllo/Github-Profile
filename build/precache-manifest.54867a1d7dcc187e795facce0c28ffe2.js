fb1f4ec956d5e29fee130c42c8ae7e38160119d1
juanpabllo <juanpablo192.dreaw@gmail.com>
2020-11-11 16:45:32 -0300

    deploy

---
+++ build/precache-manifest.54867a1d7dcc187e795facce0c28ffe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c97b35ffe079629092e0136c479d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e193f6f6e7421f7c0a00",
+    "url": "/static/css/main.bf963dd2.chunk.css"
+  },
+  {
+    "revision": "62310709faaece031c42",
+    "url": "/static/js/2.f87a2f57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f87a2f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e193f6f6e7421f7c0a00",
+    "url": "/static/js/main.8c82f3a4.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  }
+]);
