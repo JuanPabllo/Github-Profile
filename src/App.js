c4f4a0f2ab831101ce31731176079bb3c71f9a11
juanpabllo <juanpablo192.dreaw@gmail.com>
2020-11-12 12:15:31 -0300

    adding form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react'
 
 import Card from './components/card'
-import Form from './components/form'
 
 function App() {
-  var nome = prompt('Qual seu nome de usuário do GitHub?')
+  //   var nome = prompt('Qual seu nome de usuário do GitHub?')
 
   return (
     <div className='wrapper'>
-      {/* <Form /> */}
-      <Card userName={nome} />
+      <div className='forms'>
+        <form className='animate-appear'>
+          <label className='label'>Usuário GitHub</label>
+          <input
+            id='input'
+            className='input'
+            name='user'
+            placeholder=' Digite um user no GitHub'
+          />
+          <button
+            onClick={(githubName) => {
+              let input = document.getElementById('input').value
+
+              githubName = input
+              //   console.log(userName)
+            }}
+            type='submit'
+          >
+            Pesquisar
+          </button>
+        </form>
+      </div>
+      <Card userName={githubName} />
     </div>
   )
 }
